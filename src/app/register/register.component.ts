2bf836aafbaea1cafa7211abf927ee296221b818
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-07-28 14:29:23 -0400

    finshed foundation of registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, Validators } from '@angular/forms';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-register',
@@ -12,10 +13,30 @@ export class RegisterComponent implements OnInit {
   hidePassword = true;
   hideConfirmPassword = true;
 
+  user: User = new User();
+
+  confirmedPassword: String;
+
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.email,
   ]);
+
+  usernameFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  confirmPasswordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  passwordsMatch(): Boolean {
+    return this.confirmedPassword === this.user.password ? true : false;
+  }
 
   constructor(private router: Router) { }
 
