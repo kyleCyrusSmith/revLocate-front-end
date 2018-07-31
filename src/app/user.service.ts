43f8312d8f6fc136b0a37b9fdec395be26164ec4
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-07-31 15:30:28 -0400

    catching erros, but still getting some sort of post interruption

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -24,6 +24,16 @@ export class UserService {
     }
   }
 
+    public loginUser(user: User) {
+      console.log(`Attempting to login user: ${user.username}`);
+      const json = JSON.stringify(user);
+      console.log(`JSON: ` + json);
+      return this.http.post<User>(environment.apiUrl + 'user/login', json, {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+        observe: 'response'
+      });
+    }
+  /*
   public loginUser(user: User) {
     console.log(`Attempting to login user: ${user.username}`);
     const json = JSON.stringify(user);
@@ -31,9 +41,20 @@ export class UserService {
     return this.http.post<User>(environment.apiUrl + 'user/login', json, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
       observe: 'response'
+    }).map((res: Response) => {
+      if (res) {
+        if (res.status === 201) {
+          return [{ status: res.status, json: res }]
+        } else if (res.status === 200) {
+          return [{ status: res.status, json: res }]
+        }
+      }
+    }).catch((error: any) => {
+      if (error.status < 400 || error.status === 500) {
+        return Observable.throw(new Error(error.status));
+      }
     });
-  }
-
+  }*/
   public registerUser(user: User) {
     console.log(`Attempting to register user: ${user.username}`);
     const json = JSON.stringify(user);
