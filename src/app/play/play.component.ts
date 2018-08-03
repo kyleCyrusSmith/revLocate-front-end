f201ab1df6d374a025a43ef49a766e2520578b23
kylecyrussmith <kylecyrussmith@gmail.com>
2018-08-02 23:59:32 -0400

    added functionality

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -14,6 +14,16 @@ declare const google: any;
 export class PlayComponent implements OnInit {
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
+  panorama: google.maps.StreetViewPanorama;
+  created = false;
+  marker;
+  lat: number;
+  lng: number;
+  lat2: number;
+  lng2: number;
+  distance: number;
+  polyLineList;
+  polyLine: google.maps.Polyline;
 
 
   constructor(private router: Router) {
@@ -27,13 +37,7 @@ export class PlayComponent implements OnInit {
   }
 
   public makeMaps () {
-    let created = false;
-    let marker;
-    let lat: number;
-    let lng: number;
-    let lat2: number;
-    let lng2: number;
-    let panorama = new google.maps.StreetViewPanorama(
+    this.panorama = new google.maps.StreetViewPanorama(
       document.getElementById('panorama'), {
         position: { lat: 42.345573, lng: -71.098326 },
         addressControl: false,
@@ -41,11 +45,11 @@ export class PlayComponent implements OnInit {
         panControl: false,
         enableCloseButton: false
       });
-    lat2 = panorama.getPosition().lat();
-    lng2 = panorama.getPosition().lng();
-    console.log(lat2);
-    console.log(lng2);
-    let map = new google.maps.Map(
+    this.lat2 = this.panorama.getPosition().lat();
+    this.lng2 = this.panorama.getPosition().lng();
+    console.log(this.lat2);
+    console.log(this.lng2);
+    this.map = new google.maps.Map(
       document.getElementById('floatMap'), {
         center: new google.maps.LatLng(0, 0),
         zoom: 0,
@@ -54,56 +58,54 @@ export class PlayComponent implements OnInit {
         mapTypeControl: false,
         zoomControl: false
       });
-
-    google.maps.event.addListener(map, 'click', function (event) {
-      placeMarker(event.latLng);
-      console.log(lat + ' ' + lng);
+    google.maps.event.addListener(this.map, 'click', (event) => {
+      this.placeMarker2(event.latLng);
+      console.log(this.lat + ' ' + this.lng);
+      this.polyLines();
     });
 
-    function placeMarker (location) {
-      if (created) {
-        marker.setPosition(location);
-      } else {
-        created = true;
-        marker = new google.maps.Marker({
-          position: location,
-          map: map,
-          draggable: true
-        });
-      }
-      map.setCenter(marker.getPosition());
-      lat = marker.getPosition().lat();
-      lng = marker.getPosition().lng();
-      let distance = this.calcDistance(lat, lng, lat2, lng2);
-      console.log(distance);
-    }
-
   }
-  public calcDistance(lat: number, lng: number, lat2: number, lng2: number): number {
 
-    let R = 6371e3; // metres
-    let φ1 = lat * Math.PI / 180;
-    let φ2 = lat2 * Math.PI / 180;
-    let Δφ = (lat2 - lat) * Math.PI / 180;
-    let Δλ = (lng2 - lng) * Math.PI / 180;
-    let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let d = R * c;
-    return d;
+  public placeMarker2 (location) {
+    console.log('this creadted = ' + this.created);
+    if (this.created) {
+      this.marker.setPosition(location);
+    } else {
+      this.created = true;
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        draggable: true
+      });
+    }
+    this.map.setCenter(this.marker.getPosition());
+    this.lat = this.marker.getPosition().lat();
+    this.lng = this.marker.getPosition().lng();
+    this.distance = this.calcDistance(this.lat, this.lng, this.lat2, this.lng2);
+    console.log(this.distance);
+  }
+  public calcDistance (lat: number, lng: number, lat2: number, lng2: number): number {
+    const R = 6371e3; // metres
+    const φ1 = lat * Math.PI / 180;
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat) * Math.PI / 180;
+    const Δλ = (lng2 - lng) * Math.PI / 180;
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    return d / 1000;
+  }
+
+  public polyLines () {
+    this.polyLineList = [{lat: this.lat, lng: this.lng}, {lat: this.lat2, lng: this.lng2}];
+    this.polyLine = new google.maps.Polyline({
+      path: this.polyLineList,
+      geodesic: false,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    this.polyLine.setMap(this.map);
   }
 }
-
-// public placeMarker(location) {
-//   if (this.created) {
-//     this.marker.setPosition(location);
-//   } else {
-//     this.created = true;
-//     this.marker = new google.maps.Marker({
-//         position: location,
-//         map: this.map,
-//         draggable: true
-//     });
-//   }
-//   this.lat = this.marker.getPosition().lat();
-//   this.lng = this.marker.getPosition().lng();
-// }
